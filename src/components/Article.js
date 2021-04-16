6f4b3cea0423690418b451ca3f2d79e5e7f8d81f
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-16 13:40:51 +0300

    add bind funtion click

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,22 +1,35 @@
-import React from 'react';
-function Article(props){
-  const {article} = props  
-  const body = <section>{article.text}</section>
-  
-  return (
-  	<div>
-      <h2 className="top-1" style={{color:"red"}}>
-      {article.title}
-      <button onClick={handleClick}>Close</button>
-      </h2>
-      {body}
-      <h3>
-        creation date: {(new Date()).toDateString()}
-      </h3>
-    </div>
-  )
+import React, {Component} from 'react';
 
+class Article extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            isOpen: true
+        }
+
+    }
+    render(){        
+        const {article} = this.props
+        console.log('----', this.props)  
+        const body = this.state.isOpen && <section>{article.text}</section>        
+        return (
+            <div>
+                <h2 className="top-1" style={{color:"red"}}>
+                    {article.title}
+                    <button onClick={handleClick}>Close</button>
+                </h2>
+                {body}
+                <h3>
+                    creation date: {(new Date()).toDateString()}
+                </h3>
+            </div>
+        )
+
+        }
 }
+ 
+
+
 function handleClick(){
     console.log('----','clicked')
 }
