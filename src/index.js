60c9d1279e7fa5b761cff3ca4f77c1cff95433e7
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-16 12:32:20 +0300

    add article

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {render} from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Article(){
+  const body = <section>Body</section>
+  
+  return (
+  	<div>
+      <h2 className="top-1" style={{color:"red"}}>Title</h2>
+      {body}
+      <h3>
+        creation date:{(new Date).toDateString()}
+      </h3>
+    </div>
+  )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+function App(){
+  return (
+      <div>
+      	<h2>App name</h2>
+     	<Article />
+      </div>
+
+  )
+
+}
+
+render(<App/>, document.getElementById('root'))
